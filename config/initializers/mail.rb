d28999f6ffa5226393a87ea3ed11b88a52e72e81
中川涼介 <nakagawaryousuke@MBA.local>
2021-04-15 18:39:54 +0900

    fix_mailing_system8

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -10,7 +10,18 @@ if Rails.env.production?
     enable_starttls_auto: true
   }
 elsif Rails.env.development?
-  ActionMailer::Base.delivery_method = :letter_opener
+  # ActionMailer::Base.delivery_method = :letter_opener
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    port: 587,
+    user_name: 'no.reply.rental.box@gmail.com',
+    password: 'rental77',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
+
 else
   ActionMailer::Base.delivery_method = :test
 end
